e4d35985590b9ca933d36078b4822d3f619e2ce7
bakhtiyar-alvi <bakhtiyaralvi03@gmail.com>
2026-09-20 16:00:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  */
 
 // Bumped version forces Chrome to immediately install the updated index.html
-const CACHE_NAME = "streamsink-core-v6";
+const CACHE_NAME = "streamsink-core-v7";
 
 const PRECACHE_ASSETS = [
   "./",
